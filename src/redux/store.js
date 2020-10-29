9f1740ee9e966ad20f8b1a55408f46b56aa518f8
XandraV <veresalexandra95@gmail.com>
2020-10-29 18:50:49 +0000

    removing redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,8 +7,8 @@ import rootSaga from "./sagas";
 const sagaMiddleware = createSagaMiddleware();
 
 export let store = compose(
-  applyMiddleware(sagaMiddleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(sagaMiddleware)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )(createStore)(rootReducer);
 
 sagaMiddleware.run(rootSaga);
